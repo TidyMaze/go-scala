3105f61bb1fc3a006a3e1b07d373058d4148b772
yrolland <yann.rolland@iadvize.com>
2023-08-27 14:47:21 +0200

    wip

--- src/main/scala/GoAI.scala
+++ src/main/scala/GoAI.scala
@@ -1,8 +1,13 @@
 package fr.yaro.go
 
+import scala.util.Random
+
 class GoAI {
   def findBestMove(game: GoGame, state: State): Option[Move] = {
     val moves = game.getValidMoves(state)
-    moves.headOption
+    if (moves.isEmpty) None
+    else Some(randomIn(moves))
   }
+
+  private def randomIn[A](arr: Seq[A]): A = arr(Random.nextInt(arr.length))
 }
