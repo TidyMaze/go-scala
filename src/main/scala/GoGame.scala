4e8252a9a139d460341cf485c0b56a18c6133889
yrolland <yann.rolland@iadvize.com>
2023-08-27 12:51:35 +0200

    wip

--- src/main/scala/GoGame.scala
+++ src/main/scala/GoGame.scala
@@ -10,12 +10,28 @@ class GoGame {
     )
 
 
-  def play(state: State, move: Move): State = ???
+  def play(state: State, move: Move): State = {
+    state.copy(
+      grid = state.grid.updated(move.coord.y, state.grid(move.coord.y).updated(move.coord.x, Some(state.turn))),
+      turn = state.turn.opponent,
+      passed = state.passed.updated(state.turn, false)
+    )
+  }
 
   def pass(state: State): State =
     state.copy(turn = state.turn.opponent, passed = state.passed.updated(state.turn, true))
 
-  def getValidMoves(state: State): Seq[Move] = Seq.empty
+  def getValidMoves(state: State): Seq[Move] = {
+    // find any empty cell
+    val emptyCells = for {
+      (row, rowIndex) <- state.grid.zipWithIndex
+      (cell, colIndex) <- row.zipWithIndex
+      if cell.isEmpty
+    } yield Coord(colIndex, rowIndex)
+
+    emptyCells.map(Move.apply)
+
+  }
 
 
   def isOver(state: State): Boolean = {
