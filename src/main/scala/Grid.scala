486a39e8b2017fec4dd964ce241c2981c2b7ac30
yrolland <yann.rolland@iadvize.com>
2023-08-29 20:36:23 +0200

    Make Grid class parameters publicly accessible and implement equality
    Grid class's constructor parameters were made publicly accessible to use them outside the class. Also, 'equals' and 'hashCode' methods were overridden to customly determine the equality of Grid objects based on their 'grid' state, which is necessary for data comparison in certain parts of the application.

--- src/main/scala/Grid.scala
+++ src/main/scala/Grid.scala
@@ -1,6 +1,6 @@
 package fr.yaro.go
 
-class Grid(grid: Seq[Seq[Option[Color]]]) {
+class Grid(val grid: Seq[Seq[Option[Color]]]) {
   def apply(coord: Coord): Option[Color] = {
     assert(coord.x >= 0 && coord.x < size)
     assert(coord.y >= 0 && coord.y < size)
@@ -14,6 +14,19 @@ class Grid(grid: Seq[Seq[Option[Color]]]) {
   }
 
   def size: Int = grid.size
+
+
+  private def canEqual(other: Any): Boolean = other.isInstanceOf[Grid]
+  
+  override def equals(other: Any): Boolean = other match
+    case that: Grid =>
+      that.canEqual(this) &&
+        grid == that.grid
+    case _ => false
+  
+  override def hashCode(): Int =
+    val state = Seq(grid)
+    state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
 }
 
 object Grid {
