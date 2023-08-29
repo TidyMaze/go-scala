8ce1c0490cd9485a99832f321814f834c7618140
yrolland <yann.rolland@iadvize.com>
2023-08-29 19:47:54 +0200

    wip

--- src/main/scala/State.scala
+++ src/main/scala/State.scala
@@ -2,28 +2,6 @@ package fr.yaro.go
 
 import scala.util.{Failure, Try}
 
-class Grid(grid: Seq[Seq[Option[Color]]]) {
-  def apply(coord: Coord): Option[Color] = {
-    assert(coord.x >= 0 && coord.x < size)
-    assert(coord.y >= 0 && coord.y < size)
-    grid(coord.y)(coord.x)
-  }
-
-  def updated(c: Coord, color: Option[Color]): Grid = {
-    assert(c.x >= 0 && c.x < size)
-    assert(c.y >= 0 && c.y < size)
-    Grid(grid.updated(c.y, grid(c.y).updated(c.x, color)))
-  }
-
-  def size: Int = grid.size
-}
-
-object Grid {
-  def empty(size: Int): Grid = new Grid(
-    Seq.fill[Option[Color]](size, size)(None)
-  )
-}
-
 case class State(
     grid: Grid,
     turn: Color,
