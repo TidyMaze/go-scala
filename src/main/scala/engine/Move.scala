cfde637e40dafbccef402d6d8c94a7ff2e9491a5
yrolland <yann.rolland@iadvize.com>
2023-10-07 14:02:58 +0200

    cleanup

--- src/main/scala/engine/Move.scala
+++ src/main/scala/engine/Move.scala
@@ -1,8 +1,6 @@
 package fr.yaro.go
 package engine
 
-import engine.Coord
-
 import scala.util.Try
 
 case class Move(coord: Coord) {
@@ -15,16 +13,12 @@ case class Move(coord: Coord) {
 
 object Move {
 
-  // Parse a move from a string. Ex: "B3" => Move(Coord(1, 2))
-  def parse(input: String): Try[Move] = {
-    val parts = input.split("")
-    if (parts.length == 2) {
-      val x = parts(0).toUpperCase()(0) - 'A'
-      val y = parts(1).toInt - 1
-      Try(Move(Coord(x, y)))
-    } else {
-      Try(throw new Exception(s"Invalid move: $input"))
+  // Parse a move from a string. Ex: "B3" => Move(Coord(1, 2)), "G13" => Move(Coord(6, 12))
+  def parse(input: String): Try[Move] =
+    Try {
+      val x = input.charAt(0).toLower - 'a'
+      val y = input.substring(1).toInt - 1
+      Move(Coord(x, y))
     }
-  }
 
 }
